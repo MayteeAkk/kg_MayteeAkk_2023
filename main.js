8b3adf40b72e85a89187c1d201ad0cc895895fff
MayteeAkk <mayteeoakk@gmail.com>
2021-04-27 01:29:20 -0400

    Final Solution: Changed Formatting of Output, Removed Debugging Prints

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
-//HashMap to reference what word to replace each number with
+let scriptArguments = process.argv.slice(2);
+
+//HashMap to reference what word to replace each number with(Also if the Number is Negative)
 const numberConversion = new Map([
-    ['0', 'zero'],
-    ['1', 'one'],
-    ['2', 'two'],
-    ['3', 'three'],
-    ['4', 'four'],
-    ['5', 'five'],
-    ['6', 'six'],
-    ['7', 'seven'],
-    ['8', 'eight'],
-    ['9', 'nine']
+    ['-', 'Negative'],
+    ['0', 'Zero'],
+    ['1', 'One'],
+    ['2', 'Two'],
+    ['3', 'Three'],
+    ['4', 'Four'],
+    ['5', 'Five'],
+    ['6', 'Six'],
+    ['7', 'Seven'],
+    ['8', 'Eight'],
+    ['9', 'Nine']
 ]);
 
+//Function to Convert the Numbers into Words(By Digit)
 function numbertoWords(){
-    let numbers = arguments;
+    let numbers = scriptArguments;
     let main = [];
+    //Loop to Go Through Each Number in the Array that is Passed
     for(let i = 0; i < numbers.length; i++){
         let temp = [];
-        let numString = numbers[i].toString();
-        let lenofNum = numString.length;
+        let numString = numbers[i].toString(); //Converts each Number to String
+        let lenofNum = numString.length; //Find length of the Number we are looking at(e.g 52 = 2)
+        //Loop Goes Through Each Digit
         for(let j = 0; j < lenofNum; j++){
-            let word = numberConversion.get(numString[j]);
+            let word = numberConversion.get(numString[j]); //Access HashMap
             temp.push(word);
-            
         }
-        main.push(temp);
+        let combinedWords = temp.join('');
+        main.push(combinedWords);
     }
-    console.log(main);
+    console.log(main.toString());
 }
 
-numbertoWords(53,42,31,23,14,11,334);
-
+numbertoWords();
